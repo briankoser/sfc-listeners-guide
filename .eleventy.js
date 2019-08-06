b8579140f60740f91a6e86674e52e98ad273a9f3
Brian Koser <brianmkoser@gmail.com>
2019-08-06 15:39:33 -0500

    Add episodeLink nunjucks tag (fixes #129)

--- .eleventy.js
+++ .eleventy.js
@@ -41,8 +41,41 @@ module.exports = function(eleventyConfig) {
 
 
 
-  eleventyConfig.addShortcode("timeLink", timeLinkShortCode);
   eleventyConfig.addShortcode("rating", ratingShortCode);
+  eleventyConfig.addShortcode("timeLink", timeLinkShortCode);
+
+
+
+  eleventyConfig.addNunjucksTag("episodeLink", function(nunjucksEngine) {
+    return new function() {
+      this.tags = ["episodeLink"];
+
+      this.parse = function(parser, nodes, lexer) {
+        var tok = parser.nextToken();
+
+        var args = parser.parseSignature(null, true);
+        parser.advanceAfterBlockEnd(tok.value);
+
+        return new nodes.CallExtensionAsync(this, "run", args);
+      };
+
+      this.run = function(context, number, callback) {
+        let episodes = context.ctx.collections.episode.filter(e => e.data.number == number);
+        let link = '';
+
+        if (episodes.length) {
+          let {url} = episodes[0];
+          let title = episodes[0].data.title;
+          link = new nunjucksEngine.runtime.SafeString(`<a href="${url}">№ ${number} ${title}</a>`);
+        }
+        else {
+          console.log(`Episode ${number} not found in \`episodeLink\` Nunjucks tag.`);
+        }
+
+        callback(null, link);
+      };
+    }();
+  });
 
 
 
